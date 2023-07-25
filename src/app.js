3b2b1d3ae81fcc1b98e1cc6c8a343127e0d94eb7
sofi1967 <132711967+sofi1967@users.noreply.github.com>
2023-07-25 23:55:53 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,48 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.body.style.backgroundColor = "green";
+  document.body.style.height = "100vh";
+  document.body.style.display = "flex";
+  document.body.style.justifyContent = "center";
+  document.body.style.alignItems = "center";
+
+  let app = document.querySelector("#app");
+  let iconArr = ["♦", "♥", "♠", "♣"];
+  let num = ["1", "2", "3", "4", "5"];
+  let numRandom = Math.floor(Math.random() * num.length);
+  let iconRandom = Math.floor(Math.random() * iconArr.length);
+
+  app.style.height = "500px";
+  app.style.width = "300px";
+  app.style.backgroundColor = "white";
+  app.style.display = "flex";
+  app.style.flexDirection = "column";
+  app.style.alignItems = "center";
+  app.style.justifyContent = "space-between";
+
+  document.body.style.justifyContent = "center";
+  let iconTop = document.createElement("div");
+
+  iconTop.style.fontSize = "3.5rem";
+  iconTop.style.width = "100%";
+  iconTop.style.textAlign = "left";
+  iconTop.style.border = "1px solid red";
+
+  let iconMiddle = document.createElement("div");
+  iconMiddle.style.fontSize = "3.5rem";
+  let iconBotton = document.createElement("div");
+
+  iconBotton.style.fontSize = "3.5rem";
+  iconBotton.style.width = "100%";
+  iconBotton.style.textAlign = "right";
+  iconBotton.style.border = "1px solid red";
+
+  iconTop.innerHTML = iconArr[iconRandom];
+  iconMiddle.innerHTML = num[numRandom];
+  iconBotton.innerHTML = iconArr[iconRandom];
+
+  app.appendChild(iconTop);
+  app.appendChild(iconMiddle);
+  app.appendChild(iconBotton);
+  console.log(app);
 };
